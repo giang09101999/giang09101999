34750f53f099c09cac8077ca9e1742d68c4ea339
giang09101999 <Gnguyen899@gmail.com>
2023-11-26 17:04:04 +0700

    Updating the entire string

---
+++ Python/Data Types/String/Updating the entire string.py
@@ -0,0 +1,7 @@
+#Updating the entire string
+string = input("Input string: ")
+print("The original string: ", string)
+
+content = input("Input new content for string: ")
+string = content
+print("The updated string: ", string)
