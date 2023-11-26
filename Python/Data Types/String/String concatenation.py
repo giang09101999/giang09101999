ad9bfd209223894583d8b6a5f18ccbb3de1ada60
giang09101999 <Gnguyen899@gmail.com>
2023-11-26 17:19:08 +0700

    String concatenation

---
+++ Python/Data Types/String/String concatenation.py
@@ -0,0 +1,11 @@
+#String concatenation
+string_1 = input("Input string 1: ")
+print("The string 1: ", string_1)
+
+string_2 = input("Input string 2: ")
+print("The string 2: ", string_2)
+
+concatenation_string = string_1 + " " + string_2
+
+print("The concatenation String: ", concatenation_string)
+
