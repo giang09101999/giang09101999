5d35f4028316edcfa5846cf45fba2884a2b4a621
giang09101999 <Gnguyen899@gmail.com>
2023-11-26 16:57:42 +0700

    Updating a character from string

---
+++ Python/Data Types/String/Updating a character from string.py
@@ -0,0 +1,5 @@
+#Updating a character from a string
+string = "Hello World"
+print("Original string: ", string)
+updated_string = string[0:1] + "B" + string[2:]
+print("Updated string: ", updated_string)
