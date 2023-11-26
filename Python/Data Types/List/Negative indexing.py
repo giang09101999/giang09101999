b73f826e79fe2df296fe3b28dc5526f1de25127d
giang09101999 <Gnguyen899@gmail.com>
2023-11-26 20:16:41 +0700

    Negative indexing

---
+++ Python/Data Types/List/Negative indexing.py
@@ -0,0 +1,7 @@
+List = ["a", 1, 2, 'b', 10, "c", 3]
+
+print("The original list: ", List)
+print("The last element of list: ")
+print(List[-1])
+print("The third last element of list: ")
+print(List[-3])
