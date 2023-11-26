edd7ed86a3b32a006df3b4b030f2612496bf7f3d
giang09101999 <Gnguyen899@gmail.com>
2023-11-26 17:01:03 +0700

    Updating a character from string

--- Python/Data Types/String/Updating a character from string.py
+++ Python/Data Types/String/Updating a character from string.py
@@ -3,3 +3,8 @@ string = "Hello World"
 print("Original string: ", string)
 updated_string = string[0:1] + "B" + string[2:]
 print("Updated string: ", updated_string)
+
+list1 = list(string)
+list1[2] = 'N'
+new_string = ''.join(list1)
+print("Updated new string: ", new_string)
