a5555f950de939e692c69da3e63dd267b8be4962
giang09101999 <Gnguyen899@gmail.com>
2023-11-27 11:03:51 +0700

    Remove a specified element in a set

---
+++ Python/Data Types/Set/Remove a specified element in a set.py
@@ -0,0 +1,10 @@
+elements = input("Input elements: ")
+
+List = list(map(int, elements.split()))
+Set = set(List)
+
+value = int(input("Input removed value: "))
+
+print("The original set:", Set)
+Set.remove(value)
+print("The updated set:", Set)
