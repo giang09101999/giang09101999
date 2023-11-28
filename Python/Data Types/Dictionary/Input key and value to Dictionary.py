332d30f09873e81ef279f6926a1683db0cbcca01
giang09101999 <Gnguyen899@gmail.com>
2023-11-28 11:31:33 +0700

    Input key and value to Dictionary

--- Python/Data Types/Dictionary/Input key and value to Dictionary.py
+++ Python/Data Types/Dictionary/Input key and value to Dictionary.py
@@ -1,14 +1,17 @@
+import sys
 Dictionary = {}
-
-n = int(input("Input number of elements of Dictionary: "))
 
 print("1. Key: String; Value: String")
 print("2. Key: Integer; Value: Integer")
 print("3. Key: String; Value: Integer")
 print("4. Key: Integer; Value: String")
 
-mode = int(input("Choose mode: "))
+Mode = int(input("Choose mode: "))
+if ((Mode < 1) | (Mode > 4)):
+    print("Input Error!!")
+    sys.exit()
 
+n = int(input("Input number of elements of Dictionary: "))
 if mode == 1:
     for i in range(n):
         key = input("Input key: ")
@@ -16,7 +19,6 @@ if mode == 1:
         Dictionary[key] = value
     print("The dictionary:", Dictionary)
     
-
 elif mode == 2:
     for i in range(n):
         key = int(input("Input key: "))
