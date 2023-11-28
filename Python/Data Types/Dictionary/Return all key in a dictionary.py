6953802e9cdcc94c5d5412f373ef33ce7efbe188
giang09101999 <Gnguyen899@gmail.com>
2023-11-28 11:47:43 +0700

    Return all keys in a dictionary

---
+++ Python/Data Types/Dictionary/Return all key in a dictionary.py
@@ -0,0 +1,48 @@
+import sys
+
+Dictionary = {}
+
+print("1. Key: String; Value: String")
+print("2. Key: Integer; Value: Integer")
+print("3. Key: String; Value: Integer")
+print("4. Key: Integer; Value: String")
+
+Mode = int(input("Input mode: "))
+
+if ((Mode < 1) | (Mode > 4)):
+    print("Input Error!!")
+    sys.exit()
+    
+n = int(input("Input number of key-value pairs in dictionary: "))
+
+if Mode == 1:
+    for i in range(n):
+        key = input("Input key: ")
+        value = input("Input value: ")
+        Dictionary[key] = value
+
+elif Mode == 2:
+    for i in range(n):
+        key = int(input("Input key: "))
+        value = int(input("Input value: "))
+        Dictionary[key] = value
+        
+elif Mode == 3:
+    for i in range(n):
+        key = input("Input key: ")
+        value = int(input("Input value: "))
+        Dictionary[key] = value
+        
+elif Mode == 4:
+    for i in range(n):
+        key = int(input("Input key: "))
+        value = input("Input value: ")
+        Dictionary[key] = value
+        
+else:
+    print("Error!!")
+    
+print("The original dictionary: ", Dictionary)
+key = Dictionary.keys()
+
+print("Key in dictionary: ", key)
