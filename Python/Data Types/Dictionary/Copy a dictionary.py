c16c7fba2cdfb80fd6fc843434ff45d68ebf3e98
giang09101999 <Gnguyen899@gmail.com>
2023-11-30 21:39:48 +0700

    Copy a dictionary

--- Python/Data Types/Dictionary/Copy a dictionary.py
+++ Python/Data Types/Dictionary/Copy a dictionary.py
@@ -1,48 +1,57 @@
 import sys
-Dictionary = {}
 
-print("1. Key: String; Value: String")
-print("2. Key: Integer; Value: Integer")
-print("3. Key: String; Value: Integer")
-print("4. Key: Integer; Value: String")
+try:
+    Dictionary = {}
+    New_dictionary = {}
 
-Mode = int(input("Input mode: "))
-if ((Mode < 1) | (Mode > 4)):
-    print("Input Error!!")
-    sys.exit()
-    
-n = int(input("Input number of elements in a dictionary: "))
+    print("1. Key: String; Value: String")
+    print("2. Key: Integer; Value: Integer")
+    print("3. Key: String; Value: Integer")
+    print("4. Key: Integer; Value: String")
 
-if Mode == 1:
-    for i in range(n):
-        key = input("Input string key: ")
-        value = input("Input string value: ")
-        Dictionary[key] = value
-          
-elif Mode == 2:
-    for i in range(n):
-        key = int(input("Input integer key: "))
-        value = int(input("Input integer value: "))
-        Dictionary[key] = value
-
-elif Mode == 3:
-    for i in range(n):
-        key = input("Input string key: ")
-        value = int(input("Input integer value: "))
-        Dictionary[key] = value
+    Mode = int(input("Input mode: "))
+    if ((Mode < 1) | (Mode > 4)):
+        print("Input Error!!")
+        sys.exit()
         
-elif Mode == 4:
-    for i in range(n):
-        key = int(input("Input integer key: "))
-        value = input("Input string value: ")
-        Dictionary[key] = value
+    n = int(input("Input number of elements in a dictionary: "))
+
+    if Mode == 1:
+        for i in range(n):
+            key = input("Input string key: ")
+            value = input("Input string value: ")
+            Dictionary[key] = value
+              
+    if Mode == 2:
+        for i in range(n):
+            key = int(input("Input integer key: "))
+            value = int(input("Input integer value: "))
+            Dictionary[key] = value
+
+    if Mode == 3:
+        for i in range(n):
+            key = input("Input string key: ")
+            value = int(input("Input integer value: "))
+            Dictionary[key] = value
         
+    if Mode == 4:
+        for i in range(n):
+            key = int(input("Input integer key: "))
+            value = input("Input string value: ")
+            Dictionary[key] = value
+            
+    print("The original dicitonary:", Dictionary)
+    New_dictionary = Dictionary.copy()
+    print("The copied dicitonary:", New_dictionary)
+        
+except:
+    print("There are some errors: ")
+    print("1. Input wrong datatype of key")
+    print("2. Input wrong datatype of value")
+    print("3. Input wrong datatype of mode")
+    print("4. Input wrong mode")
+
 else:
-    print("Error!!")
-
-print("The original dictionary: ", Dictionary)    
-new_dictionary = Dictionary.copy()
-
-print("The copied dictionary: ", new_dictionary)
+    print("Run successfully!!")
 
 
