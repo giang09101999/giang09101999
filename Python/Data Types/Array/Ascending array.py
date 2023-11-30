e022b5f841c78c394aa7ea6bed25293beff9f0ff
giang09101999 <Gnguyen899@gmail.com>
2023-11-30 17:05:19 +0700

    Ascending array

--- Python/Data Types/Array/Ascending array.py
+++ Python/Data Types/Array/Ascending array.py
@@ -1,10 +1,9 @@
-import array as arr
 import numpy as np
 
 Elements = input("Input elements: ")
 List = list(map(int, Elements.split()))
 
-Array = arr.array("i", List)
+Array = np.array(List)
 
 print("The original array: ", Array)
 ascending_array = np.sort(Array)
