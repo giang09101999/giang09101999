7a8e76af340a5a25c26aa29e30578a264e192a19
giang09101999 <Gnguyen899@gmail.com>
2023-11-30 21:32:02 +0700

    Clear all items in dictionary

--- Python/Data Types/Dictionary/Clear all items in dictionary.py
+++ Python/Data Types/Dictionary/Clear all items in dictionary.py
@@ -1,47 +1,54 @@
 import sys
-Dictionary = {}
 
-print("1. Key: String; Value: String")
-print("2. Key: Integer; Value: Integer")
-print("3. Key: String; Value: Integer")
-print("4. Key: Integer; Value: String")
+try:
+    Dictionary = {}
 
-mode = int(input("Input mode: "))
+    print("1. Key: String; Value: String")
+    print("2. Key: Integer; Value: Integer")
+    print("3. Key: String; Value: Integer")
+    print("4. Key: Integer; Value: String")
 
-if ((mode < 1)|(mode > 4)):
-    print("Error!!")
-    sys.exit()
+    mode = int(input("Input mode: "))
 
-n = int(input("Input number of key-value pairs: "))
+    if ((mode < 1)|(mode > 4)):
+        sys.exit()
 
-if mode == 1:
-    for i in range(n):
-        key = input("Input key: ")
-        value = input("Input value: ")
-        Dictionary[key] = value
-   
-elif mode == 2:
-    for i in range(n):
-        key = int(input("Input key: "))
-        value = int(input("Input value: "))
-        Dictionary[key] = value
-        
-elif mode == 3:
-    for i in range(n):
-        key = input("Input key: ")
-        value = int(input("Input value: "))
-        Dictionary[key] = value
-        
-elif mode == 4:
-    for i in range(n):
-        key = int(input("Input key: "))
-        value = input("Input value: ")
-        Dictionary[key] = value      
+    n = int(input("Input number of key-value pairs: "))
+
+    if mode == 1:
+        for i in range(n):
+            key = input("Input key: ")
+            value = input("Input value: ")
+            Dictionary[key] = value
+       
+    if mode == 2:
+        for i in range(n):
+            key = int(input("Input key: "))
+            value = int(input("Input value: "))
+            Dictionary[key] = value
+            
+    if mode == 3:
+        for i in range(n):
+            key = input("Input key: ")
+            value = int(input("Input value: "))
+            Dictionary[key] = value
+            
+    if mode == 4:
+        for i in range(n):
+            key = int(input("Input key: "))
+            value = input("Input value: ")
+            Dictionary[key] = value
+    
+    print("The original dictionary:", Dictionary)
+    Dictionary.clear()
+    print("The cleared dictionary:" , Dictionary)
+
+except:
+    print("There are some errors: ")
+    print("1. Input wrong datatype of key")
+    print("2. Input wrong datatype of value")
+    print("3. Input wrong datatype of mode")
+    print("4. Input wrong mode")
 
 else:
-    print("Error!!")
-    
-print("The original dictionary: ", Dictionary)
-
-Dictionary.clear()
-print("The cleared dictionary:", Dictionary)
+    print("Run successfully!!")
