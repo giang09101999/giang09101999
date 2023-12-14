1c8759729b19271610a4cabd10647afc16c9715a
giang09101999 <Gnguyen899@gmail.com>
2023-12-15 03:45:03 +0700

    Check even or odd

---
+++ Python/Function/Check even or odd.py
@@ -0,0 +1,9 @@
+def check_even_or_odd(a):
+    if(a %2 == 0):
+        print("This number is even")
+    else:
+        print("This number is odd")
+
+a = int(input("Input value: "))
+
+check_even_or_odd(a)
