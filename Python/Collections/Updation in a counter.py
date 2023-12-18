87b437d6c738b585973657d5a4e5710b69efbd12
giang09101999 <Gnguyen899@gmail.com>
2023-12-18 09:06:02 -0600

    Updation of a counter

---
+++ Python/Collections/Updation in a counter.py
@@ -0,0 +1,16 @@
+from collections import Counter
+elements = input("Input elements: ")
+
+List = list(map(int, elements.split()))
+
+update_elements = input("Add elements: ")
+Update_list = list(map(int, update_elements.split()))
+
+count = Counter()
+
+count.update(List)
+print(count)
+
+count.update(Update_list)
+print(count)
+
