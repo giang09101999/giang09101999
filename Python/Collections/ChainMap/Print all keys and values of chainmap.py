0dea345a86ce8ec89596999ae920279add1c36e4
giang09101999 <Gnguyen899@gmail.com>
2023-12-19 10:09:58 -0600

    Print all keys and values of chainmap

---
+++ Python/Collections/ChainMap/Print all keys and values of chainmap.py
@@ -0,0 +1,28 @@
+from collections import ChainMap
+Dict_1 = {}
+Dict_2 = {}
+
+n1 = int(input("Input number of key-value pairs of Dictionary 1: "))
+n2 = int(input("Input number of key-value pairs of Dictionary 2: "))
+
+print("-----------------------")
+print("Input keys and values of Dictionary 1: ")
+for i in range(n1):
+    key = int(input("Input key: "))
+    value = input("Input value: ")
+    Dict_1[key] = value
+
+print("------------------------")
+print("Input keys and values of Dictionary 2: ")
+for i in range(n2):
+    key = int(input("Input key: "))
+    value = input("Input value: ")
+    Dict_2[key] = value
+
+c = ChainMap(Dict_1, Dict_2)
+
+print("=========================")
+print("The chainmap:", c)
+print("The key:", list(c.keys()))
+print("The value:", list(c.values()))
+
