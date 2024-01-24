65454a4faa5efc575ed2592a13f383d408a38a93
giang09101999 <Gnguyen899@gmail.com>
2024-01-24 11:08:18 -0600

    Size of operator

---
+++ C++/Data_Type/Operators/Sizeof_Operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+    std::cout << "Size of int is: " << sizeof(int) << std::endl;
+    std::cout << "Size of float is: " << sizeof(float) << std::endl;
+    std::cout << "Size of double is: " << sizeof(double) << std::endl;
+    std::cout << "Size of short is: " << sizeof(short) << std::endl;
+    std::cout << "Size of char is: " << sizeof(char) << std::endl;
+    std::cout << "Size of long is: " << sizeof(long) << std::endl;
+}
