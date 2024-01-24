0a09cde9e5eaccc3b4de0e193d01e5c17684c930
giang09101999 <Gnguyen899@gmail.com>
2024-01-24 11:14:37 -0600

    Size of data type taken by variables

---
+++ C++/Data_Type/Operators/Sizeof_datatype_taken_by_variable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main()
+{
+    int a = 1;
+    double b = 5;
+    long c = 9;
+    short d = 10;
+    char e = 'Giang';
+
+    std::cout << "Size of datatype a is: " << sizeof(a) << std::endl;
+    std::cout << "Size of datatype b is: " << sizeof(b) << std::endl;
+    std::cout << "Size of datatype c is: " << sizeof(c) << std::endl;
+    std::cout << "Size of datatype d is: " << sizeof(d) << std::endl;
+    std::cout << "Size of datatype e is: " << sizeof(e) << std::endl;
+}
