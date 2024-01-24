7678c992b45a1a0cb4464948603cf51a792bd532
giang09101999 <Gnguyen899@gmail.com>
2024-01-24 10:44:33 -0600

    Explicit type conversion

--- C++/Data_Type/Data Type Conversion/Explicit_Type_Conversion.cpp
+++ C++/Data_Type/Data Type Conversion/Explicit_Type_Conversion.cpp
@@ -8,4 +8,7 @@ int main()
     double c = double(a) + double(b);
 
     std::cout << "The value of c is: " << c << std::endl;
+    std::cout << "The datatype of a is: " << typeid(a).name() << std::endl;
+    std::cout << "The datatype of b is: " << typeid(b).name() << std::endl;
+    std::cout << "The datatype of c is: " << typeid(c).name() << std::endl;
 }
