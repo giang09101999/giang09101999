5ed3be55fb2273a5a196775dff213bcd9eda74e0
giang09101999 <Gnguyen899@gmail.com>
2024-01-30 10:11:20 -0600

    Sum sub mul div of two numbers

---
+++ C++/Parameter/Sum_sub_mul_div.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+void Addiction(float A, float B, float Sum)
+{
+    Sum = A + B;
+    std::cout << "Sum: " << Sum << std::endl;
+}
+
+void Substraction(float A, float B, float Sub)
+{
+    Sub = A - B;
+    std::cout << "Substraction: " << Sub << std::endl;    
+}
+
+void Multiply(float A, float B, float Mul)
+{
+    Mul = A * B;
+    std::cout << "Multiply: " << Mul << std::endl;    
+}
+
+void Division(float A, float B, float Div)
+{
+    Div = A / B;
+    std::cout << "Division: " << Div << std::endl;    
+}
+
+int main()
+{
+    float a, b, sum, sub, mul, div;
+    std::cout << "Enter a: ";
+    std::cin >> a;    
+    
+    std::cout << "Enter b: ";
+    std::cin >> b;
+
+    Addiction(a, b, sum);
+    Substraction(a, b, sub);
+    Multiply(a, b, mul);
+    Division(a, b, div);
+}
