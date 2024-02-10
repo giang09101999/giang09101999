8d36649db2632c10cbd21a4d0b4969d68f50a3e6
giang09101999 <Gnguyen899@gmail.com>
2024-02-09 21:23:16 -0600

    Size of array

---
+++ C++/Data_structure/Array/Size_of_array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+int main()
+{
+    int n, i;
+    std::cout << "Enter number of elements of array: ";
+    std::cin >> n;
+
+    int array[n];
+    for(i = 0; i < n; i++)
+    {
+        std::cout << "Array[" << i << "] = ";
+        std::cin >> array[i];
+
+    }
+
+    std::cout << "=======================" << std::endl;
+    for(i = 0; i < n; i++)
+    {
+        std::cout << "Array[" << i << "] = " << array[i] << std::endl;
+
+    }
+
+    std::cout << "The size of array: " << sizeof(array) << std::endl;
+}
