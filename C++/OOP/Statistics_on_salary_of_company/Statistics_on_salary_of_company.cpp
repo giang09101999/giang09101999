5daf97c95276828e5bab81e8ce52c457b0b500a0
giang09101999 <Gnguyen899@gmail.com>
2024-03-13 15:57:49 -0500

    Statistics on salary of company

--- C++/OOP/Statistics_on_salary_of_company/Statistics_on_salary_of_company.cpp
+++ C++/OOP/Statistics_on_salary_of_company/Statistics_on_salary_of_company.cpp
@@ -1,22 +1,23 @@
 #include<iostream>
 #include<string>
+using namespace std;
 class Salary
 {
-public:
+
     double employee_number, manager_number, director_number;
     double employee_salary, manager_salary, director_salary;
     double revenue, cost;
-    Salary(double revenue)
+public:
+    Salary()
     {
-        std::cout << "Enter revenue: ";
-        std::cin >> revenue;
-        this->revenue = revenue; 
-        std::cout << "Enter number of employees: ";
-        std::cin >> employee_number;
-        std::cout << "Enter number of managers: ";
-        std::cin >> manager_number;
-        std::cout << "Enter number of directors: ";
-        std::cin >> director_number;        
+        cout << "Enter revenue: ";
+        cin >> revenue;
+        cout << "Enter number of employees: ";
+        cin >> employee_number;
+        cout << "Enter number of managers: ";
+        cin >> manager_number;
+        cout << "Enter number of directors: ";
+        cin >> director_number;        
         cost = (revenue*30)/100;
         employee_salary = (((cost*70)/100)/employee_number);
         manager_salary = (((cost*15)/100)/manager_number);
@@ -25,20 +26,20 @@ public:
     }
     void Display()
     {
-        std::string Cost = std::to_string(cost);
-        std::string employee_string = std::to_string(employee_salary);
-        std::string mananger_string = std::to_string(manager_salary);
-        std::string director_string = std::to_string(director_salary);
-        std::cout << "Cost: " << Cost << std::endl; 
-        std::cout << "Salary of each employees: " << employee_string << std::endl;
-        std::cout << "Salary of each managers: " << mananger_string << std::endl;
-        std::cout << "Salary of each directors: " << director_string << std::endl;
+        string Cost = to_string(cost);
+        string employee_string = to_string(employee_salary);
+        string mananger_string = to_string(manager_salary);
+        string director_string = to_string(director_salary);
+        cout << "Cost: " << Cost << endl; 
+        cout << "Salary of each employees: " << employee_string << endl;
+        cout << "Salary of each managers: " << mananger_string << endl;
+        cout << "Salary of each directors: " << director_string << endl;
     }
 };
 
 int main()
 {   
-    double revenue;
-    Salary salary(revenue);
+
+    Salary salary;
     salary.Display();
 }
